b5e3a470628ec42cd9111f90c9dda82acc74b122
AdrineUwera <uweraadrine3@gmail.com>
2024-01-26 23:43:42 +0200

    landing page and some dashboard sections

---
+++ pages/dev-dashboard/transactions.js
@@ -0,0 +1,237 @@
+import React, { useState } from "react";
+
+export default function DashboardTransactions() {
+  return (
+    <div class="flex h-full items-center">
+      <div class="sticky top-0 inset-x-0 z-20 bg-white border-y px-4 sm:px-6 md:px-8 lg:hidden ">
+        <div class="flex items-center py-4">
+          <button
+            type="button"
+            class="text-gray-500 hover:text-gray-600"
+            data-hs-overlay="#application-sidebar"
+            aria-controls="application-sidebar"
+            aria-label="Toggle navigation"
+          >
+            <span class="sr-only">Toggle Navigation</span>
+            <svg
+              class="w-5 h-5"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+              />
+            </svg>
+          </button>
+          <ol
+            class="ms-3 flex items-center whitespace-nowrap"
+            aria-label="Breadcrumb"
+          >
+            <li class="flex items-center text-sm text-gray-800 ">
+              Application Layout
+              <svg
+                class="flex-shrink-0 mx-3 overflow-visible h-2.5 w-2.5 text-gray-400 "
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M5 1L10.6869 7.16086C10.8637 7.35239 10.8637 7.64761 10.6869 7.83914L5 14"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                />
+              </svg>
+            </li>
+            <li
+              class="text-sm font-semibold text-gray-800 truncate"
+              aria-current="page"
+            >
+              Dashboard
+            </li>
+          </ol>
+        </div>
+      </div>
+      <div
+        id="application-sidebar"
+        class="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 "
+      >
+        <div class="px-6">
+          <a
+            class="flex-none text-xl font-semibold"
+            href="#"
+            aria-label="Brand"
+          >
+            <div class="flex items-center space-x-2">
+              <div class="text-4xl font-bold text-blue-500">Sway</div>
+            </div>
+          </a>
+        </div>
+
+        <nav
+          class="hs-accordion-group p-6 w-full flex flex-col flex-wrap space-y-48"
+          data-hs-accordion-always-open
+        >
+          <ul class="space-y-1.5">
+            <li>
+              <a
+                class="w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent text-sm text-slate-700 rounded-lg hover:bg-gray-100 "
+                href="/dev-dashboard"
+              >
+                <svg
+                  class="flex-shrink-0 w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                  <polyline points="9 22 9 12 15 12 15 22" />
+                </svg>
+                Dashboard
+              </a>
+            </li>
+
+            <li class="hs-accordion" id="account-accordion">
+              <a
+                href="/dev-dashboard/projects"
+                class="  w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent text-sm text-slate-700 rounded-lg hover:bg-gray-100 "
+              >
+                <svg
+                  class="flex-shrink-0 mt-0.5 w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <circle cx="18" cy="15" r="3" />
+                  <circle cx="9" cy="7" r="4" />
+                  <path d="M10 15H6a4 4 0 0 0-4 4v2" />
+                  <path d="m21.7 16.4-.9-.3" />
+                  <path d="m15.2 13.9-.9-.3" />
+                  <path d="m16.6 18.7.3-.9" />
+                  <path d="m19.1 12.2.3-.9" />
+                  <path d="m19.6 18.7-.4-1" />
+                  <path d="m16.8 12.3-.4-1" />
+                  <path d="m14.3 16.6 1-.4" />
+                  <path d="m20.7 13.8 1-.4" />
+                </svg>
+                Projects
+              </a>
+            </li>
+
+            <li class="hs-accordion" id="projects-accordion">
+              <a
+                href="/dev-dashboard/transactions"
+                class="flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-sm text-slate-700 rounded-lg hover:bg-gray-100"
+              >
+                <svg
+                  class="flex-shrink-0 w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <rect width="20" height="14" x="2" y="7" rx="2" ry="2" />
+                  <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16" />
+                </svg>
+                Transactions
+              </a>
+            </li>
+
+            <li>
+              <a
+                class="w-full flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-lg hover:bg-gray-100 "
+                href="/dev-dashboard/demo-requests"
+              >
+                <svg
+                  class="flex-shrink-0 w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <rect width="18" height="18" x="3" y="4" rx="2" ry="2" />
+                  <line x1="16" x2="16" y1="2" y2="6" />
+                  <line x1="8" x2="8" y1="2" y2="6" />
+                  <line x1="3" x2="21" y1="10" y2="10" />
+                  <path d="M8 14h.01" />
+                  <path d="M12 14h.01" />
+                  <path d="M16 14h.01" />
+                  <path d="M8 18h.01" />
+                  <path d="M12 18h.01" />
+                  <path d="M16 18h.01" />
+                </svg>
+                Demo Requests
+              </a>
+            </li>
+
+            <li class="hs-accordion" id="users-accordion">
+              <button
+                type="button"
+                class="hs-accordion-toggle w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent text-sm text-slate-700 rounded-lg hover:bg-gray-100"
+              >
+                <svg
+                  class="flex-shrink-0 w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                  <circle cx="9" cy="7" r="4" />
+                  <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+                  <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+                </svg>
+                Account
+              </button>
+            </li>
+          </ul>{" "}
+          <a
+            href="/"
+            class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-blue-600 text-blue-600 hover:border-blue-500 hover:text-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+          >
+            Log out
+          </a>
+        </nav>
+        <div class="w-full flex flex-col flex-wrap"></div>
+      </div>
+      <div class="w-full pt-10 px-4 sm:px-6 md:px-8 lg:ps-72">
+        <p class="pb-6 text-2xl font-semibold text-blue-600">Demo schedule</p>
+        <div>
+            
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+export default function HeroSection() {
+  return (
+    <>
+      <Navbar />
+      <div class="bg-white">
+        <div class="relative isolate px-6 py-14 lg:px-8">
+          <div
+            class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            aria-hidden="true"
+          >
+            <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
+          </div>
+          <div class="mx-auto max-w-2xl py-24">
+            <div class="hidden sm:mb-8 sm:flex sm:justify-center">
+              <div class="relative rounded-full px-3 py-1 mb-4 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                Ready to buy or elevate a project?{" "}
+                <a href="/projects" class="font-semibold text-indigo-600">
+                  <span class="absolute inset-0" aria-hidden="true"></span>Find
+                  your next project
+                  <span aria-hidden="true">&rarr;</span>
+                </a>
+              </div>
+            </div>
+            <div class="text-center">
+              <div class="mt-5 max-w-2xl text-center mx-auto">
+                <h1 class="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-800">
+                  Discover.
+                  <span class="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent mx-4">
+                    Sell.
+                  </span>
+                  Revitalize.
+                </h1>
+              </div>
+              <div class="mt-5 max-w-3xl text-center mx-auto ">
+                <p class="text-lg text-gray-800 dark:text-gray-400">
+                  Sway is where developers transform their passion into value.
+                  Whether you're looking to sell a groundbreaking project or
+                  revive a dormant one, this is your platform.
+                </p>
+              </div>
+              <div class="mt-10 flex items-center justify-center gap-x-6">
+                <a
+                  href="/login"
+                  class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Get started
+                </a>
+                <a
+                  href="#benefits"
+                  class="text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Learn more <span aria-hidden="true">→</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true"
+          >
+            <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2  opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"></div>
+          </div>
+        </div>
+
+        <div class="bg-white pb-24 pt-2 sm:pb-32" id="benefits">
+          <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:text-center">
+              <h2 class="text-base font-semibold leading-7 text-indigo-600">
+                Benefits
+              </h2>
+              <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                Why use Sway
+              </p>
+              <p class="mt-6 text-lg leading-8 text-gray-600">
+                Join Sway and take control of your creations. This is where
+                innovation finds value, and dormant projects discover a new
+                life!
+              </p>
+            </div>
+            <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+              <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                <div class="relative pl-16">
+                  <dt class="text-base font-semibold leading-7 text-gray-900">
+                    <div class="absolute left-0 top-0 flex h-10 w-10 text-white items-center justify-center rounded-lg bg-indigo-600">
+                      1
+                    </div>
+                    Monetize Your Genius
+                  </dt>
+                  <dd class="mt-2 text-base leading-7 text-gray-600">
+                    Showcase your projects to a large audience. From innovative
+                    masterpieces to dormant gems, every creation has value.
+                  </dd>
+                </div>
+                <div class="relative pl-16">
+                  <dt class="text-base font-semibold leading-7 text-gray-900">
+                    <div class="absolute left-0 top-0 flex h-10 w-10 text-white items-center justify-center rounded-lg bg-indigo-600">
+                      2
+                    </div>
+                    Revive Dormant Projects
+                  </dt>
+                  <dd class="mt-2 text-base leading-7 text-gray-600">
+                    Breathe new life into your forgotten projects. Let others
+                    takeover and contribute to your work, turning potential into
+                    reality.
+                  </dd>
+                </div>
+                <div class="relative pl-16">
+                  <dt class="text-base font-semibold leading-7 text-gray-900">
+                    <div class="absolute left-0 top-0 flex h-10 w-10 text-white items-center justify-center rounded-lg bg-indigo-600">
+                      3
+                    </div>
+                    Connect with Buyers
+                  </dt>
+                  <dd class="mt-2 text-base leading-7 text-gray-600">
+                    Our platform connects you with potential buyers, fostering a
+                    marketplace where developers and projects find their perfect
+                    match.
+                  </dd>
+                </div>
+                <div class="relative pl-16">
+                  <dt class="text-base font-semibold leading-7 text-gray-900">
+                    <div class="absolute left-0 top-0 flex h-10 w-10 text-white items-center justify-center rounded-lg bg-indigo-600">
+                      4
+                    </div>
+                    Secure Transactions
+                  </dt>
+                  <dd class="mt-2 text-base leading-7 text-gray-600">
+                    Built-in security measures to ensure safe transactions,
+                    protecting both sellers and buyers.
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
